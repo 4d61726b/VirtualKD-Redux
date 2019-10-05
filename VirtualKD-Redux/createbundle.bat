7839179d7ef5fe05e4ccc21c6a9e44ee58259630
Mark <55967456+4d61726b@users.noreply.github.com>
2019-10-04 22:01:59 -0400

    Added the new property sheet to kdpatch and fixed up a few other projects.

--- VirtualKD-Redux/createbundle.bat
+++ VirtualKD-Redux/createbundle.bat
@@ -3,7 +3,7 @@ copy Build\Win32\Release\kdclient32.dll Bundle\
 copy Build\x64\Release\kdclient64.dll Bundle\
 copy Build\Win32\Release\vmmon32.exe Bundle\
 copy Build\x64\Release\vmmon64.exe Bundle\
-copy Build\Win32\Release\vmxpatch.exe Bundle\
+copy Build\Win32\Release\vmxpatch32.exe Bundle\
 copy Build\x64\Release\vmxpatch64.exe Bundle\
 copy Build\AnyCPU\Release\VirtualBoxIntegration.exe Bundle\
 mkdir Bundle\target32
