20acbe02e96011f2782dfb2659b5ce12bcdb2be3
4d61726b <55967456+4d61726b@users.noreply.github.com>
2020-05-09 00:17:43 -0400

    Removed kdpatch from the bundle since this was never being used or maintained and was causing problems (See issue #11)

--- VirtualKD-Redux/createbundle.bat
+++ VirtualKD-Redux/createbundle.bat
@@ -8,12 +8,12 @@ copy Build\x64\Release\vmxpatch64.exe Bundle\
 copy Build\AnyCPU\Release\VirtualBoxIntegration.exe Bundle\
 mkdir Bundle\target32
 mkdir Bundle\target64
-copy Build\Win32\Release\kdpatch.reg Bundle\target32
-copy Build\x64\Release\kdpatch.reg Bundle\target64
+REM copy Build\Win32\Release\kdpatch.reg Bundle\target32
+REM copy Build\x64\Release\kdpatch.reg Bundle\target64
 copy Build\Win32\Release\vminstall.exe Bundle\target32
 copy Build\x64\Release\vminstall.exe Bundle\target64
 copy Build\Win32\Release\kdbazis.dll Bundle\target32
 copy Build\x64\Release\kdbazis.dll Bundle\target64
-copy Build\Win32\Release\kdpatch.sys Bundle\target32
-copy Build\x64\Release\kdpatch.sys Bundle\target64
+REM copy Build\Win32\Release\kdpatch.sys Bundle\target32
+REM copy Build\x64\Release\kdpatch.sys Bundle\target64
 
