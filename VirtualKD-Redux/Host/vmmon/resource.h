a838885fff5835e1efa61f4a7e766fa1079c314f
4d61726b <55967456+4d61726b@users.noreply.github.com>
2020-10-08 23:35:55 -0400

    Fixed resizing issues in vmmon in regards to various controls.

--- VirtualKD-Redux/Host/vmmon/resource.h
+++ VirtualKD-Redux/Host/vmmon/resource.h
@@ -42,6 +42,7 @@
 #define IDC_LOGHINT                     1017
 #define IDC_USEWINDBGPREVIEW            1017
 #define IDC_WINDBGPREVIEWPATH           1018
+#define IDC_FRAME                       1019
 
 // Next default values for new objects
 // 
@@ -49,7 +50,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        210
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_CONTROL_VALUE         1020
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
