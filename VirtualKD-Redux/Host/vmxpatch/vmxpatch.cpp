87ad431d7406639a551b8d9a37d732a9bf98aaba
4d61726b <55967456+4d61726b@users.noreply.github.com>
2021-09-11 07:15:58 -0400

    Cleaned up and fixed various issues in vmxpatch

--- VirtualKD-Redux/Host/vmxpatch/vmxpatch.cpp
+++ VirtualKD-Redux/Host/vmxpatch/vmxpatch.cpp
@@ -9,6 +9,34 @@
 #include "vkdversion.h"
 #include <conio.h>
 
+static void Pause(bool exit = true)
+{
+    printf("\nPress any key to %s...\n", exit ? "exit" : "continue");
+    _getche();
+}
+
+static bool AskYesOrNoQuestion(const char* question)
+{
+    int c;
+
+    for (;;)
+    {
+        printf("%s (y/n) ", question);
+        c = tolower(_getche());
+        if (c == 'y' || c == 'n')
+        {
+            break;
+        }
+        if (c != '\n')
+        {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+    return c == 'y';
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     printf("%s %s\n\n", PRODUCTNAME_STR, VIRTUALKD_REDUX_VERSION_STR);
@@ -17,6 +45,7 @@ int _tmain(int argc, _TCHAR* argv[])
     if (hSnap == INVALID_HANDLE_VALUE)
     {
         printf("Unexpected error!\n");
+        Pause();
         return -1;
     }
 
@@ -36,6 +65,7 @@ int _tmain(int argc, _TCHAR* argv[])
     if (!PIDCount)
     {
         printf("Cannot find vmware-vmx.exe running!\n");
+        Pause();
         return 1;
     }
 
@@ -46,31 +76,60 @@ int _tmain(int argc, _TCHAR* argv[])
         GetVMSessionNameW(PIDs[i], tszName, sizeof(tszName) / sizeof(tszName[0]));
         printf("%d\t%d\t%S\t%s\n", i + 1, PIDs[i], tszName, IsVMSessionPatched(PIDs[i]) ? "(already patched)" : "");
     }
-    printf("------------------------------------\nWhich VM session to patch? (1-%d):", PIDCount);
 
-    unsigned idx = _getche() - '1';
-    printf("\n");
-    if (idx >= (unsigned)PIDCount)
+    printf("------------------------------------\n");
+
+    unsigned idx = UINT_MAX;
+    while (idx >= (unsigned)PIDCount)
     {
-        printf("Invalid index specified.\n");
-        return 2;
+        printf("Which VM session to patch/unpatch? (1-%d): ", PIDCount);
+        idx = _getche() - '1';
+        printf("\n");
+        if (idx >= (unsigned)PIDCount)
+        {
+            printf("Invalid index specified.\n");
+        }
     }
 
-    if (!PatchVMSessionIfNeeded(PIDs[idx]))
+    if (IsVMSessionPatched(PIDs[idx]))
+    {
+        if (UnpatchVMSessionIfNeeded(PIDs[idx]))
+        {
+            printf("Failed to unpatch VM session.\n");
+            Pause();
+            return 3;
+        }
+
+        printf("The vmware-vmx.exe has been successfully unpatched.\n");
+        Pause();
+        return 0;
+    }
+
+    if (PatchVMSessionIfNeeded(PIDs[idx]))
     {
         printf("Failed to patch VM session.\n");
+        Pause();
         return 3;
     }
 
-    if (MessageBox(0, _T("The vmware-vmx.exe has been successfully patched. You can now use the debugger. Do you want to unpatch it back?"), _T("VM patcher"), MB_ICONQUESTION | MB_YESNO) != IDYES)
+    if (!AskYesOrNoQuestion("The vmware-vmx.exe has been successfully patched. You can now use the debugger.\nDo you want to unpatch it back?"))
+    {
+        Pause();
         return 0;
+    }
 
-    if (!UnpatchVMSessionIfNeeded(PIDs[idx]))
+    if (UnpatchVMSessionIfNeeded(PIDs[idx]))
     {
         printf("Failed to unpatch VM session.\n");
+        Pause();
         return 3;
     }
+    else
+    {
+        printf("The vmware-vmx.exe has been successfully unpatched.\n");
+    }
 
+    Pause();
     return 0;
 }
 
