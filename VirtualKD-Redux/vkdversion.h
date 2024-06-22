a17c0adc397d2c3b48625073d5cb5d4dc02226fc
4d61726b <55967456+4d61726b@users.noreply.github.com>
2024-06-22 08:50:17 -0400

    Bumped version up to 2024.1

--- VirtualKD-Redux/vkdversion.h
+++ VirtualKD-Redux/vkdversion.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#define VIRTUALKD_REDUX_VERSION         2024,0
-#define VIRTUALKD_REDUX_VERSION_STR     "2024.0"
+#define VIRTUALKD_REDUX_VERSION         2024,1
+#define VIRTUALKD_REDUX_VERSION_STR     "2024.1"
 #define COPYRIGHT_STR                   "LGPL"
 #define TRADEMARKS_STR                  "VirtualKD-Redux"
 #define PRODUCTNAME_STR                 "VirtualKD-Redux"
 #define COMPANY_STR                     "VirtualKD-Redux"
 #define COMMENTS_STR                    "https://github.com/4d61726b/VirtualKD-Redux"
 
-#define VIRTUALKD_REDUX_VER_INT         0x07e80000
+#define VIRTUALKD_REDUX_VER_INT         0x07e80001
 #define KDVM_DLL_MIN_VER_INT            0x07e50000
 
 #define KDCLIENT_DLL_VER_STR            VIRTUALKD_REDUX_VERSION_STR
